ff45bf7c4ea4ef7260fb0857d60aab65186b9fbd
rushils372 <59312459+rushils372@users.noreply.github.com>
2022-02-19 22:12:09 +0530

    first commit

---
+++ src/InputShortener.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InputShortener = () => {
+  return (
+    <div className='inputContainer'>
+        <h1>URL <span>Shortener</span></h1>
+        <div>
+            <input type="text" placeholder='Paste a link to shorten it'/>
+            <button>Shorten</button>
+        </div>
+    </div>
+  )
+}
+
+export default InputShortener
